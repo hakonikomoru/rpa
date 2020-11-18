908a4708cb348604f2c5828cf8a2b08f9b151de4
ken.ebata <ebatadevelop@gmail.com>
2020-11-18 20:21:30 +0900

    楽天EnaviPointゲットrpa追加

--- PythonUITest/PointGet/rakutenENaviClick.py
+++ PythonUITest/PointGet/rakutenENaviClick.py
@@ -2,8 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-from pages import LoginPage
-from pages import MyAuctionPage
+from pages import RakutenENaviLoginPage
+from pages import ClickDePointPage
 import chromedriver_binary
 
 # 落札された商品のすべての取引評価を行う
@@ -12,16 +12,15 @@ import chromedriver_binary
 # options.add_argument('--headless')
 # driver = webdriver.Chrome(options=options)
 driver = webdriver.Chrome()
-loginPage = LoginPage(driver)
-loginPage.open()
-loginPage.ログイン('e_ba_ta_8122@yahoo.co.jp','Kensyo01')
-myAuctionPage = MyAuctionPage(loginPage.driver)
-myAuctionPage.出品終了分画面をURLで直接開く()
-myAuctionPage.評価ボタンを押下()
+rakutenENaviLoginPage = RakutenENaviLoginPage(driver)
+rakutenENaviLoginPage.open()
+rakutenENaviLoginPage.ログイン('syokkotan','hnhn8787')
+clickDePointPage = ClickDePointPage(rakutenENaviLoginPage.driver)
+clickDePointPage.aタグをすべてクリック()
 
 # アップツールも自動化視野
 # https://apptool.jp/mypage
 # もし確認するボタンがあった場合、前の画面に戻って次のループ
 #　↑出ない場合は、評価を行う
 
-myAuctionPage.close()
+# myAuctionPage.close()
